535dff9e0f400482a7f8162f6618ffde14483f51
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-21 00:24:33 +0700

    js lab 115

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// write code here
+// ให้สร้าง array result จาก array ที่กำหนด โดยใช้ arr.map(fn)
+
+const array = [1, 2, 30, 400];
+const array2 = array.map(item => item * 2);
+console.log(array2);
+// result: [2, 4, 60, 800"]
+
+const array = [1, 2, 3, 4];
+const array2 = array.map(item => item.toString())
+console.log(array2);
+// result: ["1", "2", "3", "4"]
+
+const array = [1, '1', 2, {}];
+const array2 = array.map(item => typeof(item))
+console.log(array2)
+// result: ["number", "string", "number", "object"]
